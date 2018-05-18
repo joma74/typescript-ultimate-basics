ec801ae9125fa8837a1bed8f3cda9e8f18f50ef6
joma74 <dev-mails@gmx.net>
2018-05-18 23:10:23 +0200

    feat: next chapters per ts file

--- src/destructObjArray.ts
+++ src/destructObjArray.ts
@@ -12,3 +12,14 @@ function doOrder({ name: pizzaName, toppings }: {name: string, toppings: string[
 console.log(doOrder(doPizza));
 
 const doToppings = ['pepperoni', 'bacon', 'chilli'];
+
+// destructuring into a virtual array...
+const [first, second, third] = doToppings;
+// ...where each item is assigned to a variable within
+console.log(first, second, third);
+
+function doLogToppings([first, second, third]: [any, any, any]){
+    console.log(first, second, third);
+}
+
+console.log(doToppings);
