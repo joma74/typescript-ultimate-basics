4a2982bc64beef2429b90e5c878196b8ceb96d67
joma74 <dev-mails@gmx.net>
2018-05-21 23:38:21 +0200

    feat: next chapters per ts file

--- src/arrTypesGenericsTupleTypes.ts
+++ src/arrTypesGenericsTupleTypes.ts
@@ -15,3 +15,28 @@ let atgttPizzaSize: atgttSize = "small";
 type f_atgttSelectSize = (size: atgttSize) => void;
 
 const atgttSelectSize: f_atgttSelectSize = size => (atgttPizzaSize = size);
+
+// casting 
+
+type atgttOType_Pizza = { name: string, toppings: number };
+
+const atgttO_Pizza: atgttOType_Pizza = {
+  name: "Pepperoni",
+  toppings: 4
+};
+
+// casting with <template>
+const atgttSerialized = JSON.stringify(atgttO_Pizza);
+
+function getNameFromJSON1(json: string): string {
+  return (<atgttOType_Pizza>JSON.parse(json)).name;
+}
+
+console.log(getNameFromJSON1(atgttSerialized));
+
+// casting with "as"
+function getNameFromJSON2(json: string): string {
+    return (JSON.parse(json) as atgttOType_Pizza).name;
+  }
+  
+  console.log(getNameFromJSON2(atgttSerialized));
