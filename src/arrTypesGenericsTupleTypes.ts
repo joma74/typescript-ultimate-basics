36a8ce4205d02b9b7c500ed1d0ebd0116486d63d
joma74 <dev-mails@gmx.net>
2018-05-23 00:30:40 +0200

    feat: realized as is a brutal/hard cast

--- src/arrTypesGenericsTupleTypes.ts
+++ src/arrTypesGenericsTupleTypes.ts
@@ -1,42 +1,42 @@
-let atgttToppings: Array<string>;
+let atgttToppings: Array<string>
 
-atgttToppings = ["pepperoni", "tomato", "bacon"];
+atgttToppings = ["pepperoni", "tomato", "bacon"]
 
 // tuple type
-let atgttPizza: [string, number];
+let atgttPizza: [string, number]
 
-atgttPizza = ["Peperoni", 20];
+atgttPizza = ["Peperoni", 20]
 
 // types
-type atgttSize = "small" | "medium" | "large";
+type atgttSize = "small" | "medium" | "large"
 
-let atgttPizzaSize: atgttSize = "small";
+let atgttPizzaSize: atgttSize = "small"
 
-type f_atgttSelectSize = (size: atgttSize) => void;
+type f_atgttSelectSize = (size: atgttSize) => void
 
-const atgttSelectSize: f_atgttSelectSize = size => (atgttPizzaSize = size);
+const atgttSelectSize: f_atgttSelectSize = (size) => (atgttPizzaSize = size)
 
-// casting 
+// casting
 
-type atgttOType_Pizza = { name: string, toppings: number };
+type atgttOType_Pizza = { name: string; toppings: number }
 
 const atgttO_Pizza: atgttOType_Pizza = {
   name: "Pepperoni",
-  toppings: 4
-};
-
-// casting with <template>
-const atgttSerialized = JSON.stringify(atgttO_Pizza);
-
-function getNameFromJSON1(json: string): string {
-  return (<atgttOType_Pizza>JSON.parse(json)).name;
+  toppings: 4,
 }
 
-console.log(getNameFromJSON1(atgttSerialized));
+// casting with <template>
+const atgttSerialized = JSON.stringify(atgttO_Pizza)
 
-// casting with "as"
+function getNameFromJSON1(json: string): string {
+  return (<atgttOType_Pizza>JSON.parse(json)).name
+}
+
+console.log(getNameFromJSON1(atgttSerialized))
+
+// brutal/hard casting with "as"
 function getNameFromJSON2(json: string): string {
-    return (JSON.parse(json) as atgttOType_Pizza).name;
-  }
-  
-  console.log(getNameFromJSON2(atgttSerialized));
+  return (JSON.parse(json) as atgttOType_Pizza).name
+}
+
+console.log(getNameFromJSON2(atgttSerialized))
